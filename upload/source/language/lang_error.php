488e4b1a19bf1aebbe2f83dca74ca9d16b995433
Naruebet <nac_naruebet@hotmail.com>
2022-09-15 20:15:41 +0700

    Update Language

--- upload/source/language/lang_error.php
+++ upload/source/language/lang_error.php
@@ -33,8 +33,8 @@ $lang = array
 	'system_init_ok' => 'ระบบเว็บไซต์เริ่มต้นได้โปรด<a href="index.php">คลิกที่นี่</a>เข้า',
 	'backtrace' => 'เรียกใช้ข้อมูล',
 	'error_end_message' => '<a href="http://{host}">{host}</a> บันทึกรายละเอียดของข้อมูลข้อผิดพลาดนี้, ความไม่สะดวกในการเข้าถึงคุณขออภัย',
-	'suggestion_plugin' => 'หากคุณเป็นเว็บมาสเตอร์ฉันขอแนะนำให้คุณลองปิดที่ศูนย์บริหารจัดการ <a href="admin.php?action=plugins&frames=yes" class="guess" target="_blank">{guess}</a> เสียบเข้าไป <a href="admin.php?action=tools&operation=updatecache&frames=yes" class="guess" target="_blank">รีเฟรชแคช</a> แก่นแท้หากปัญหาได้รับการแก้ไขหลังจากปลั๊กในขอแนะนำให้คุณติดต่อปลั๊กอินปลั๊กเพื่อขอความช่วยเหลือ',
-	'suggestion' => 'หากคุณเป็นเว็บมาสเตอร์ขอแนะนำให้คุณลองไปที่ศูนย์บริหารจัดการ <a href="admin.php?action=tools&operation=updatecache&frames=yes" target="_blank">รีเฟรชแคช</a> คุณยังสามารถผ่าน <a href="https://www.discuz.net/" target="_blank">Discuz! เป็นทางการ</a> ขอความช่วยเหลือหากคุณแน่ใจว่านี่เป็น Bug คุณสามารถเป็นได้โดยตรง <a href="https://gitee.com/discuz/DiscuzX/issues" target="_blank">ส่ง Issue</a> ให้เรา',
+	'suggestion_plugin' => 'หากคุณเป็นเว็บมาสเตอร์ขอแนะนำให้คุณลอง <a href="admin.php?action=plugins&frames=yes" class="guess" target="_blank">{guess}</a> เพื่อเช็คปลั๊คอินหรือ <a href="admin.php?action=tools&operation=updatecache&frames=yes" class="guess" target="_blank">รีเฟรชแคช</a> หากปัญหายังไม่ได้รับการแก้ไขขอแนะนำให้คุณติดต่อยังเจ้าของปลั๊กอินปลั๊กเพื่อขอความช่วยเหลือ',
+	'suggestion' => 'หากคุณเป็นเว็บมาสเตอร์ขอแนะนำให้คุณลอง <a href="admin.php?action=tools&operation=updatecache&frames=yes" target="_blank">รีเฟรชแคช</a> สามารถขอความช่วยเหลือผ่าน <a href="https://www.discuz.net/" target="_blank">เว็บไซต์อย่างเป็นทางการของ Discuz! </a> หากคุณแน่ใจว่านี่เป็น Bug คุณสามารถเข้าไป <a href="https://gitee.com/discuz/DiscuzX/issues" target="_blank">แจ้งปัญหา</a> ให้เราทราบได้',
 
 	'file_upload_error_-101' => 'การอัพโหลดล้มเหลว!ไฟล์อัปโหลดไม่มีอยู่หรือผิดกฎหมายโปรดส่งคืน',
 	'file_upload_error_-102' => 'การอัพโหลดล้มเหลว!สำหรับไฟล์ประเภทที่ไม่ใช่รูปภาพโปรดส่งคืน',

b18fbe2e0b714141509c71015d56ffa5566cdd96
Naruebet <nac_naruebet@hotmail.com>
2022-09-29 16:45:29 +0700

    P20220929

--- upload/source/function/media/media_youtube.php
+++ upload/source/function/media/media_youtube.php
@@ -10,14 +10,7 @@ function media_youtube($url, $width, $height) {
 	if(preg_match("/^https?:\/\/(|m.|www.)youtube.com\/watch\?v=([^\/&]+)&?/i", $url, $matches)) {
 		$flv = 'https://www.youtube.com/v/'.$matches[2].'&fs=1';
 		$iframe = 'https://www.youtube.com/embed/'.$matches[2];
-		if(!$width && !$height) {
-			$str = dfsockopen($url);
-			if(!empty($str) && preg_match("/'VIDEO_HQ_THUMB':\s'(.+?)'/i", $str, $image)) {
-				$url = substr($image[1], 0, strrpos($image[1], '/')+1);
-				$filename = substr($image[1], strrpos($image[1], '/')+3);
-				$imgurl = $url.$filename;
-			}
-		}
+		$imgurl = 'https://i3.ytimg.com/vi/'.$matches[2].'/maxresdefault.jpg';
 	}
 	return array($flv, $iframe, $url, $imgurl);
 }
